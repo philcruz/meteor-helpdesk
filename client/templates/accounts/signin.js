5cf5cd59d2b84e5b43d1b92a2e254667ef745f30
Phil Cruz <phil@philcruz.com>
2015-03-06 14:53:03 -0800

    implement sign in

---
+++ client/templates/accounts/signin.js
@@ -0,0 +1,10 @@
+Template.signin.events({
+    'submit #signin': function(event,template){
+        var emailVar = template.find('#email').value;
+        var passwordVar = template.find('#password').value;
+        console.log("email: " + emailVar);
+        console.log("Form submitted.");
+        event.preventDefault();
+        Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+});
