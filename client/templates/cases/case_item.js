ce5c2c424347be65099433cd9b5064b0f2ef50bd
Phil Cruz <phil@philcruz.com>
2015-03-11 16:32:40 -0700

    remove from the collection on swipe

--- client/templates/cases/case_item.js
+++ client/templates/cases/case_item.js
@@ -2,5 +2,14 @@ Template.caseItem.events({
     'swipeable-card-swipe-away swipeable-card': function(e){
         //alert('wooho');
         e.target.parentNode.removeChild(e.target);
+        var id = $(e.target).find('[name=id]').val();
+        console.log(id);
+        Cases.remove(id);
+    },
+    
+    'click swipeable-card': function(e){
+        //alert('clicked swipeable-card');
+        var id = $(e.target).find('[name=id]').val();
+        console.log(id);        
     }
 });
