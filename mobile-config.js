39495d023bfbd5e163d696654f085c9f7e29b0ad
Phil Cruz <phil@philcruz.com>
2015-03-23 14:30:02 -0700

    add platforms: iOS and android, update to 1.0.4.2

---
+++ mobile-config.js
@@ -0,0 +1,8 @@
+App.info({
+  id: 'com.philcruz.meteor-helpdesk',
+  name: 'MeteorHD',
+  description: 'HelpDesk app built with Meteor',
+  author: 'Phil Cruz',
+  email: 'phil@philcruz.com',
+  website: 'http://philcruz.com'
+});
