dfec06da09b3aca49b5892fffd0532cbdad7bb9c
Phil Cruz <phil@graphpad.com>
2015-04-29 11:13:11 -0700

    implement reply by email, settings.json

--- client/templates/cases/view_case.js
+++ client/templates/cases/view_case.js
@@ -1,28 +1,51 @@
+function caseFromForm(form){
+    var title = $(form).find('[name=title]').val();
+    var question = $(form).find('[name=question]').val();
+    var answer = $(form).find('[name=answer]').val();    
+    var assignedTo = document.querySelector('#assignedTo').selectedItemLabel;
+    return {title: title, question: question, answer: answer, assignedTo: assignedTo};
+}
+
+
 Template.viewcase.events({
     'click #cancel-button': function(){
         Router.go('casesList');
     },
     
     'click #save-button': function(event){
-        console.log('clicked to save case...');
-        console.log(event);
+        console.log('clicked to save case...');        
+        
         var form = $.find('#caseform');
-        //console.log(form);
-        //var newCase = newCaseFromForm(form);
-        //newCase._id = Cases.insert(newCase);    
-        //Router.go('casesList');
-        
-        
-        var caseID = this._id;
-        var caseProperties = {            
-            title: $(form).find('[name=title]').val(),
-            question: $(form).find('[name=question]').val(),
-            answer: $(form).find('[name=answer]').val(),
-            assignedTo: document.querySelector('#assignedTo').selectedItemLabel
-        };
-        
-        Cases.update( caseID, {$set: caseProperties}, function (error){} );
+        var caseID = this._id;        
+        var thisCase = caseFromForm(form);
+        //var thisCase = {            
+        //    title: $(form).find('[name=title]').val(),
+        //    question: $(form).find('[name=question]').val(),
+        //    answer: $(form).find('[name=answer]').val(),
+        //    assignedTo: document.querySelector('#assignedTo').selectedItemLabel
+        //};
+        Cases.update( caseID, {$set: thisCase}, function (error){} );        
         Router.go('casesList');
+    },
+    
+    'click #send-button': function(event){   
+        console.log("clicked send button...");
+        
+        //update the case
+        var form = $.find('#caseform');
+        var caseID = this._id;        
+        var thisCase = caseFromForm(form);
+        Cases.update( caseID, {$set: thisCase}, function (error){} );  
+        
+        //send the email
+        var questionLines = thisCase.question.split("\n");        
+        var emailBody = thisCase.answer + "\n";
+        for(var i = 0;i < questionLines.length;i++){
+            emailBody = emailBody + ">" + questionLines[i] + "\n";
+        }
+        Meteor.call('sendEmail', "RE:"+ thisCase.title , emailBody );        
+        Router.go('casesList');
+
     },
     
     'submit form': function(e){
--- server/app.js
+++ server/app.js
@@ -1,5 +1,14 @@
 
 Meteor.startup(function () {
-    // code to run on server at startup
+    console.log("app.js:startup...");
+    process.env.MAIL_URL = Meteor.settings.mailUrl;
 });
 
+sendEmail = function (subject, text) {
+  Email.send({
+    from: Meteor.settings.fromEmail,
+    to: "phil@philcruz.com",    
+    subject: subject,
+    text: text
+  });
+}
