33f8346be01256280ff6fec844388a2e9fb399ce
Zachary Hunsaker <admin@linux4learning.org>
2016-03-18 10:21:02 -0600

    Created a Name element in the signup section and added it to the users collection. in a field called profile (this is for use with the account module being used.) User name in the template page is then populated with the name they entered on signup.

--- client/main.js
+++ client/main.js
@@ -4,6 +4,13 @@ getCurrentUserEmail = function (){
     var user = Meteor.user();
     if (user && user.emails)
         return user.emails[0].address;
-}
+};
+
+getUsersName = function () {
+    var user = Meteor.user();
+    if (user && user.profile)
+        return user.profile; 
+}; 
 
 Template.registerHelper('currentUserEmail', getCurrentUserEmail);
+Template.registerHelper('usersProfileName', getUsersName); 
